2688e57cd0030e1cc192834b47468e3bc7e28ef2
Julio3021 <julio_antaurco@usmp.pe>
2024-09-27 15:58:51 -0500

    Primer commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace G_R_Industrias_Plasticas.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
